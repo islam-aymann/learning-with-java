9ca0ace00852e09378cd6055c4c08dc91dd5bfe2
Islam Ayman <e-i.mohammed@lean.sa>
2021-05-18 05:54:22 +0200

    CodewithmoshDS&AL1: add LinkedLists

--- src/com/islam/Main.java
+++ src/com/islam/Main.java
@@ -1,96 +1,24 @@
 package com.islam;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        hasCycle();
-    }
+        LinkedList<Integer> list = new LinkedList<>();
 
-    public static void regularGraph() {
-        var graph = new Graph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
+        list.add(10);
+        list.add(20);
+        list.add(30);
 
-        graph.addEdge("A", "B");
-        graph.addEdge("A", "C");
-
-        graph.print();
-
-//        graph.removeEdge("A", "C");
-//        graph.print();
-
-//        graph.removeEdge("A", "D");
-//        graph.print();
-
-//        graph.removeNode("B");
-//        graph.print();
-
-//        graph.removeNode("A");
-//        graph.print();
-
-        graph.addEdge("B", "C");
-        graph.removeNode("A");
-        graph.print();
-    }
-
-    public static void depthFirstRecursive() {
-        var graph = new Graph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "D");
-        graph.addEdge("D", "C");
-        graph.addEdge("A", "C");
-
-        graph.traverseDepthFirstRecursive("C");
-    }
-
-    public static void depthFirstIteration() {
-        var graph = new Graph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "D");
-        graph.addEdge("D", "C");
-        graph.addEdge("A", "C");
-
-        graph.traverseDepthFirstIteration("A");
-    }
-
-    public static void topologicalSort() {
-        var graph = new Graph();
-        graph.addNode("X");
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("P");
-
-        graph.addEdge("X", "A");
-        graph.addEdge("X", "B");
-        graph.addEdge("A", "P");
-        graph.addEdge("B", "P");
-
-        var list = graph.topologicalSort();
+        list.remove(0);
+        list.addFirst(5);
         System.out.println(list);
-    }
 
-    public static void hasCycle() {
-        var graph = new Graph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
-
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "C");
-        graph.addEdge("C", "A");
-
-        System.out.println(graph.hasCycle());
+        System.out.println(list.indexOf(2));
+        System.out.println(list.indexOf(5));
+        System.out.println(list.size());
+        System.out.println(Arrays.toString(list.toArray()));
     }
 
 
