1297b773b0bb3a82c932158eea5670f44b729ea5
Islam Ayman <e-i.mohammed@lean.sa>
2021-05-18 06:26:20 +0200

    CodewithmoshDS&AL1: add Stacks

--- src/com/islam/Main.java
+++ src/com/islam/Main.java
@@ -1,25 +1,10 @@
 package com.islam;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-
-        list.add(10);
-        list.add(20);
-        list.add(30);
-
-        list.remove(0);
-        list.addFirst(5);
-        System.out.println(list);
-
-        System.out.println(list.indexOf(2));
-        System.out.println(list.indexOf(5));
-        System.out.println(list.size());
-        System.out.println(Arrays.toString(list.toArray()));
+        String str = "(1 + 2)";
+        Expression exp = new Expression();
+        var result = exp.isBalanced(str);
+        System.out.println(result);
     }
-
-
 }
